01cabb0ac3aa520ecc3a77784f594bd7467755c6
nonny9 <123nonkung@gmail.com>
2023-08-30 04:41:55 +0700

    04.41 30/08/2566

--- pages/filmcamera.js
+++ pages/filmcamera.js
@@ -19,7 +19,7 @@ export default function filmcamera() {
    <div className="container">
     <Link className="navbar-brand" Link href="/."> 
       <img src="iconnav1.png" alt="Bootstrap" width={50} height={50} />&nbsp;  
-      Film camera
+      N_IT
       </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
@@ -29,17 +29,10 @@ export default function filmcamera() {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" link href="/.">Index</Link>
         </li>
-        <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Product
-          </Link>
-          <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" Link href="filmcamera">Film camera</Link></li>
-            <li><Link className="dropdown-item" Link href="film">Film </Link></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><Link className="dropdown-item" Link href="lens">Lens</Link></li>
-          </ul>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" link href="filmcamera">Product</Link>
         </li>
+        
         <li className="nav-item">
           <Link className="nav-link" Link href="about">About</Link>
         </li>
@@ -202,70 +195,6 @@ export default function filmcamera() {
   <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
 </svg> </button>
                 </div>
-                <small className="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card shadow-sm">
-          <img src="fc7.jpg" alt="Bootstrap" width="100%" height={225} />
-            <div className="card-body">
-              <p className="card-text">กล้องฟิล์ม Rollei 35 SE  (ค.ศ. 1980) </p>
-              <h1><p className="card-text"> 12,000 ฿ </p></h1>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
-</svg> </button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-</svg> </button>
-                </div>
-                <small className="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card shadow-sm">
-          <img src="fc8.jpg" alt="Bootstrap" width="100%" height={225} />
-            <div className="card-body">
-              <p className="card-text">กล้องฟิล์ม Leica Mini Lux (ค.ศ. 2005) </p>
-              <h1><p className="card-text"> 35,000 ฿ </p></h1>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
-</svg> </button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-</svg> </button>
-                </div>
-                <small className="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card shadow-sm">
-          <img src="fc9.png" alt="Bootstrap" width="100%" height={225} />
-            <div className="card-body">
-              <p className="card-text">กล้องฟิล์ม YASHICA ELECTRO 35 GS (ค.ศ.1970) </p>
-              <h1><p className="card-text"> 7,890 ฿ </p></h1>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
-</svg> </button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-</svg> </button>
-                </div>
-                <small className="text-body-secondary">9 mins</small>
               </div>
             </div>
           </div>
@@ -273,84 +202,66 @@ export default function filmcamera() {
       </div>
     </div>
   </div>
-  <footer className="text-body-secondary py-5">
-    <div className="container">
-      <p className="float-end mb-1">
-        <a href="#">Back to top</a>
-      </p>
-      <p className="mb-1">Album example is © Bootstrap, but please download and customize it for yourself!</p>
-      <p className="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.3/getting-started/introduction/">getting started guide</a>.</p>
-    </div>
-  </footer>
-</div>
-
+  </div>
 </main>
 
 <footer>
-  
-<div>
-  <div className="b-example-divider" />
-  <div className="container">
-    <footer className="py-5">
-      <div className="row">
-        <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div>
-        <div className="col-md-5 offset-md-1 mb-3">
-          <form>
-            <h5>Subscribe to our newsletter</h5>
-            <p>Monthly digest of what's new and exciting from us.</p>
-            <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-              <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-              <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-              <button className="btn btn-primary" type="button">Subscribe</button>
-            </div>
-            <div className="d-flex flex-column flex-sm-row w-100 gap-2">  
-              <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
-            </div>
-          </form>
-        </div>
-      </div>
-      <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p>© 2022 Company, Inc. All rights reserved.</p>
-        <ul className="list-unstyled d-flex">
-          <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
-          <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
-          <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
+<div className="container">
+  <footer className="py-5">
+    <div className="row">
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
         </ul>
       </div>
-    </footer>
-  </div>
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+      <div className="col-md-5 offset-md-1 mb-3">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of what's new and exciting from us.</p>
+          <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+            <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
+            <button className="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p>© 2023 Company, Inc. All rights reserved.</p>
+      <ul className="list-unstyled d-flex">
+        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
+        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
+        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
+      </ul>
+    </div>
+</footer>
 </div>
-
-</footer> 
+</footer>
     </>
   )
 }
--- pages/support.js
+++ pages/support.js
@@ -18,7 +18,7 @@ export default function support() {
    <div className="container">
     <Link className="navbar-brand" Link href="/."> 
       <img src="iconnav1.png" alt="Bootstrap" width={50} height={50} />&nbsp;  
-      Film camera
+      N_IT
       </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
@@ -28,17 +28,10 @@ export default function support() {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" link href="/.">Index</Link>
         </li>
-        <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Product
-          </Link>
-          <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" Link href="filmcamera">Film camera</Link></li>
-            <li><Link className="dropdown-item" Link href="film">Film </Link></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><Link className="dropdown-item" Link href="lens">Lens</Link></li>
-          </ul>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" link href="filmcamera">Product</Link>
         </li>
+        
         <li className="nav-item">
           <Link className="nav-link" Link href="about">About</Link>
         </li>
@@ -162,70 +155,62 @@ export default function support() {
 </main>
 
 <footer>
-  
-<div>
-  <div className="b-example-divider" />
-  <div className="container">
-    <footer className="py-5">
-      <div className="row">
-        <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div>
-        <div className="col-md-5 offset-md-1 mb-3">
-          <form>
-            <h5>Subscribe to our newsletter</h5>
-            <p>Monthly digest of what's new and exciting from us.</p>
-            <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-              <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-              <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-              <button className="btn btn-primary" type="button">Subscribe</button>
-            </div>
-            <div className="d-flex flex-column flex-sm-row w-100 gap-2">  
-              <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
-            </div>
-          </form>
-        </div>
-      </div>
-      <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p>© 2022 Company, Inc. All rights reserved.</p>
-        <ul className="list-unstyled d-flex">
-          <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
-          <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
-          <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
+<div className="container">
+  <footer className="py-5">
+    <div className="row">
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
         </ul>
       </div>
-    </footer>
-  </div>
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+      <div className="col-md-5 offset-md-1 mb-3">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of what's new and exciting from us.</p>
+          <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+            <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
+            <button className="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p>© 2023 Company, Inc. All rights reserved.</p>
+      <ul className="list-unstyled d-flex">
+        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
+        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
+        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
+      </ul>
+    </div>
+</footer>
 </div>
-
-</footer> 
-</>
+</footer>
+    </>
   )
 }
